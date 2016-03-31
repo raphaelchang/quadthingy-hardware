d5b7bffd01c9148a4769aae1d7c9c0df9387a30f
Anurag Makineni <anurag.makineni@gmail.com>
2016-03-31 00:56:33 -0400

    calculations for controller done, need to add force publisher

--- simulator/controller/src/motors.cpp
+++ simulator/controller/src/motors.cpp
@@ -5,7 +5,10 @@
 
 void motors_cb(const controller::Motors::ConstPtr& msg)
 {
-  ROS_INFO("Got motors message!");
+  //get quad charectaristics
+  float d = msg->d;
+  float kf = msg->kf;
+  float kt = msg->kt;
 
   //get motor speeds (rad/s)
   const Eigen::Vector4d motor_speeds(
@@ -14,7 +17,15 @@ void motors_cb(const controller::Motors::ConstPtr& msg)
       msg->motor3,
       msg->motor4);
 
-  const Eigen::Vector4d motor_thrust = msg->kf * motor_speeds.cwiseProduct(motor_speeds);
+  //calculate forces and moments
+  const Eigen::Vector4d motor_thrust = kf * motor_speeds.cwiseProduct(motor_speeds);
+  Eigen::Matrix4d convert;
+  convert << 1, 1, 1, 1,
+             0, d, 0, -d,
+             -d, 0, d, 0,
+             kt, -kt, kt, -kt;
+  
+  const Eigen::Vector4d f_moments = convert * motor_thrust;
 
 }
 
@@ -25,6 +36,7 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
 
   ros::Subscriber sub = n.subscribe("/motors", 1, motors_cb);
+
 
   ros::spin();
 
