defcd38b83824276050aa8f53cdc06161fe7afb5
raphaelchang <raphaelchang1227@gmail.com>
2017-01-08 19:52:55 -0800

    Move code to other repo

--- code/ChibiOS_16.1.4/community/os/hal/ports/STM32/STM32F0xx/platform.mk
+++
@@ -1,8 +0,0 @@
-include ${CHIBIOS}/os/hal/ports/STM32/STM32F0xx/platform.mk
-
-PLATFORMSRC += ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD/CRCv1/crc_lld.c \
-               ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD/TIMv1/timcap_lld.c \
-
-PLATFORMINC += ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD/CRCv1 \
-               ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD/TIMv1 \
-               ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD
--- code/adc2.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ADC2_H_
-#define _ADC2_H_
-
-#include <stdint.h>
-
-void adc_init(void);
-float adc_get_sys_voltage(void);
-
-#endif /* _ADC_H_ */
